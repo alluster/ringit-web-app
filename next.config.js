23c305101b54864b611b12576b0a586a64ec25d7
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-05-19 16:11:02 +0300

    remove rinki

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = withLess({
 		AUTHO_CLIENT_ID: process.env.AUTHO_CLIENT_ID,
 		AUTHO_REDIRECT_URI: process.env.AUTHO_REDIRECT_URI,
 		AUTHO_RETURN_URL: process.env.AUTHO_RETURN_URL,
+		BASE_URL: process.env.BASE_URL,
 		DATABASE_HOST: process.env.DATABASE_HOST,
 		DATABASE_USER: process.env.DATABASE_USER,
 		DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
